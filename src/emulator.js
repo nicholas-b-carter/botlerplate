877e3d78a70b39a6cee93c251085406097364cdb
François Triquet <francois.triquet@recast.ai>
2016-10-05 18:16:30 +0200

    make emulator requires directory

--- src/emulator.js
+++ src/emulator.js
@@ -1,8 +1,11 @@
 import readline from 'readline'
+import _ from 'lodash'
 import mongoose from 'mongoose'
+import requireAll from 'require-all'
 
-import Action from './action'
 import Bot from './bot'
+
+const actions = requireAll(`${__dirname}/actions`)
 
 const recastToken = ''
 
@@ -10,128 +13,6 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 })
-
-class Greetings extends Action {
-  constructor() {
-    super()
-    this.intent = 'greetings'
-  }
-
-  reply() {
-    return { en: ['Hello! Do you already have an account ?'],
-             fr: ['Bien le bonjour! Avez vous deja un compte chez nous ?'] }
-  }
-}
-
-class Yes extends Action {
-  constructor() {
-    super()
-    this.intent = 'yes'
-    this.next = 'InfosSignin'
-  }
-  reply() {
-    return ['Nice! Good to see you again! I just need some informations before we continue']
-  }
-}
-
-class No extends Action {
-  constructor() {
-    super()
-    this.intent = 'no'
-    this.next = 'InfosSignup'
-  }
-  reply() {
-    return ['Fair enough! Let\'s create one together. I just need some basic informations.']
-  }
-}
-
-class Signup extends Action {
-  constructor() {
-    super()
-    this.intent = 'signup'
-    this.next = 'InfosSignup'
-  }
-  reply() {
-    return { en: ['Alright let\'s do it'] }
-  }
-}
-
-class Signin extends Action {
-  constructor() {
-    super()
-    this.intent = 'signin'
-    this.next = 'InfosSignin'
-  }
-  reply() {
-    return { en: ['Alright let\'s do it'] }
-  }
-}
-
-class InfosSignup extends Action {
-  constructor() {
-    super()
-    this.intent = 'infos'
-    this.dependencies = [{
-      actions: ['No', 'Signup'],
-      isMissing: ['Do you already have an account ?'],
-    }]
-    this.constraints = [{
-      entities: [{ entity: 'person', alias: 'client' }],
-      isMissing: ['How should I call you ?'],
-    }, {
-      entities: [{ entity: 'email', alias: 'mail-client' }],
-      isMissing: ['What\'s the email address I can join you at?'],
-    }]
-    this.next = 'Murder'
-  }
-  reply() {
-    return { en: ['Perfect {{client}}, let\'s go on'] }
-  }
-}
-
-class InfosSignin extends Action {
-  constructor() {
-    super()
-    this.intent = 'infos'
-    this.dependencies = [{
-      actions: ['Yes', 'Signin'],
-      isMissing: ['Do you already have an account ?'],
-    }]
-    this.constraints = [{
-      entities: [{ entity: 'person', alias: 'client' }],
-      isMissing: ['What\'s the name you are registered with?'],
-    }, {
-      entities: [{ entity: 'email', alias: 'mail-client' }],
-      isMissing: ['What\'s your email address ?'],
-    }]
-    this.next = 'Murder'
-  }
-  reply() {
-    return { en: ['Perfect {{client}}, let\'s go on.'] }
-  }
-}
-
-class Murder extends Action {
-  constructor() {
-    super()
-    this.intent = 'murder'
-    this.dependencies = [{
-      actions: ['InfosSignup', 'InfosSignin'],
-      isMissing: ['Do you already have an account?'],
-    }]
-    this.constraints = [{
-      entities: [{ entity: 'location', alias: 'lieu' }],
-      isMissing: ['Where can I find the target?'],
-    }, {
-      entities: [{ entity: 'person', alias: 'victim' }],
-      isMissing: ['Who do you want me to kill?'],
-    }]
-  }
-  reply() {
-    return { en: ['Wonderful! {{victim}} will soon be buried. How do you want us to operate? We can eliminate him by burning or fire arms.'] }
-  }
-}
-
 
 const token = recastToken || process.env.TOKEN || process.argv[2]
 
@@ -143,19 +24,18 @@ const bot = new Bot({
   },
 })
 
-bot.registerActions([Greetings, Signin, Signup, InfosSignin, InfosSignup, Murder, Yes, No])
+bot.registerActions(_.values(actions))
 
 bot.useDatabase({
   hostname: 'localhost',
   port: '27017',
-  name: 'test-bb',
+  name: 'test',
 })
 
 
 process.stdin.setEncoding('utf8')
 
 /* eslint no-console: "off" */
-
 
 const conversId = Math.floor((Math.random() * 1000) + 1).toString()
 
@@ -183,5 +63,7 @@ rl.on('line', input => {
     process.stdout.write('> ')
   }).catch(err => {
     console.log(`Error: ${err}`)
+    console.log()
+    process.stdout.write('> ')
   })
 })
