eb106e38b0b6a2f4cf3cde8403c7137a675b98dd
François Triquet <francois.triquet@recast.ai>
2016-10-19 17:05:36 +0200

    [UPD] examples + move tests to dialog-manager

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  recastToken: '91f9255be83ad17be2e7a098d2069672',
+  recastToken: '',
   database: {
-    hostname: 'localhost',
-    port: '27017',
-    name: 'test-bot',
+    hostname: '',
+    port: '',
+    name: '',
   },
 }
