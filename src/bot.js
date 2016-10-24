e1a7d71b1b9e616e9d08329094c57e1e49a9b9ba
François Triquet <francois.triquet@recast.ai>
2016-10-24 18:30:02 +0200

    add bot.sj

---
+++ src/bot.js
@@ -0,0 +1,23 @@
+import _ from 'lodash'
+import requireAll from 'require-all'
+
+import { Bot } from 'bot-dialog-manager'
+import config from '../config'
+
+const actions = requireAll(`${__dirname}/actions`)
+
+const recastToken = ''
+
+const token = recastToken || config.recastToken || process.env.TOKEN || process.argv[2]
+
+const myBot = new Bot({
+  token,
+  fallbackReplies: {
+    en: ['Aye donte endeurstende'],
+    fr: ['Moi yen a pas comprendre'],
+  },
+})
+
+myBot.registerActions(_.values(actions))
+
+module.export = myBot
